69cc706690f30e25bd3bd555b5265e172f70a9fc
Howaida <h.sayed@neoxero.com>
2024-10-07 01:41:09 +0300

    fetching related products

--- controllers/admin.js
+++ controllers/admin.js
@@ -11,19 +11,11 @@ exports.getAddProduct = (req, res, next) => {
 
 // adding a new product 
 exports.postAddProduct = (req, res, next) => {
-    // create an object based on the class blueprint
-    console.log("====================");
-    console.log(req.user);
-    console.log("====================");
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    /* 
-        create a new associated object
-        since a user has many products
-        ==> this automatically creates a connected model
-    */
+
     req.user.createProduct({
         title: title,
         imageUrl: imageUrl,
@@ -34,16 +26,6 @@ exports.postAddProduct = (req, res, next) => {
     }).catch(err => {
         console.log(err)
     });
-
-    // sequelize create method to create new element & store it to db (1 step)
-    // build does the same but create it in js first & then save it to database (2 steps)
-    //Product.create({
-    //title: title,
-    //imageUrl: imageUrl,
-    //price: price,
-    //description: description,
-    //userId: req.user.id // create a product with a user associate to it (here's manually)
-    //})
 }
 // edit product 
 exports.getEditProduct = (req, res, next) => {
@@ -53,18 +35,20 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect("/");
     }
     const prodId = req.params.productId;
-
-    Product.findByPk(prodId).then((product) => {
-        if (!product) {
-            return res.redirect("/");
-        }
-        res.render("admin/edit-product", {// render view 
-            path: "/admin/edit-product",
-            pageTitle: "Edit Product",
-            editing: editMode,
-            product: product
-        });
-    }).catch(err => console.log(err))
+    // get products for that user 
+    req.user.getProducts({ where: { id: prodId } })
+        //Product.findByPk(prodId)
+        .then((products) => {
+            if (!products) {
+                return res.redirect("/");
+            }
+            res.render("admin/edit-product", {// render view 
+                path: "/admin/edit-product",
+                pageTitle: "Edit Product",
+                editing: editMode,
+                product: products[0]
+            });
+        }).catch(err => console.log(err))
 }
 
 exports.postEditProduct = (req, res, next) => {
@@ -93,21 +77,20 @@ exports.postEditProduct = (req, res, next) => {
         })
         // this will catch error for 1st & 2nd promise
         .catch(err => console.log(err))
-
-    // const updatedProduct = new Product(id, title, imageUrl, price, description);
-    // // save the product 
 }
 
 // adding a new product 
 exports.getProducts = (req, res, next) => {
-    // then & catch because we use promises
-    Product.findAll().then((product) => {
-        res.render("admin/products", {// render view 
-            path: "/admin/products",
-            pageTitle: "Admin products",
-            prods: product
-        });
-    }).catch((err) => { console.log(err) })
+    req.user.getProducts()
+        // then & catch because we use promises
+        //Product.findAll()
+        .then((product) => {
+            res.render("admin/products", {// render view 
+                path: "/admin/products",
+                pageTitle: "Admin products",
+                prods: product
+            });
+        }).catch((err) => { console.log(err) })
 }
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -119,3 +102,4 @@ exports.postDeleteProduct = (req, res, next) => {
         console.log(err)
     });
 }
+
