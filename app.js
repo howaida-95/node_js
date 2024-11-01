6d6b78525e05d3462d8434f3888cf1d0b6717be6
Howaida <h.sayed@neoxero.com>
2024-11-01 20:57:56 +0200

    Finishing the Database Connection

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const { mongoConnect } = require('./util/database');
 
 const app = express();
 
@@ -22,7 +22,6 @@ app.use("/admin", adminRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(client => {
-    console.log(client);
+mongoConnect(() => {
     app.listen(3000);
 });
--- util/database.js
+++ util/database.js
@@ -1,20 +1,62 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient; // MongoClient used to connect mongodb 
+
+// underscore _ ==> means that only used internally in this file 
+let _db;
+
 // This code defines a function `mongoConnect` that connects to a MongoDB database using the MongoDB client. 
 // Upon a successful connection, it logs the result and invokes a callback function with the client object. 
 // In case of an error during the connection, it logs the error message.
 
 const mongoConnect = (callback) => {
-    MongoClient.connect("mongodb+srv://howaida:1751995@cluster0.awxto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MongoClient.connect(
+        "mongodb+srv://howaida:1751995@cluster0.awxto.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+    )
         .then((client) => {
             // client obj --> give me access to database
             console.log(res, "res");
+            _db = client.db(); // store connection to db variable
             callback(client);
         }).catch((err) => {
             console.log(err, "err");
+            throw err;
         })
 }
-module.exports = mongoConnect;
+
+const getDb = () => {
+    if (_db) {
+        return _db; // return db -> return access to db 
+    }
+    throw "No database found"
+}
+//module.exports = mongoConnect;
+/* 
+This module exports two functions: 
+1. mongoConnect
+================ 
+which are likely used for 
+connecting to a MongoDB database & store this connection to database 
+
+2.getDb
+========
+retrieving the database instance, respectively.
+return access to that connection to db if that exist
+
+==> mongodb will manage that elegantly with -> connection pool 
+mongodb provides sufficient connections for multiple simultaneous interactions with the db 
+
+==> according to that need to adjust app.js 
+
+mongoConnect(client => {
+    console.log(client);
+    app.listen(3000);
+});
+
+here we don't get client anymore 
+*/
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
 /*
 bad way to connect to mongoDB
