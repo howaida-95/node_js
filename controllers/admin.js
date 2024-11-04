0e0029750f09c1c6065ad25b095be484cded1f09
Howaida <h.sayed@neoxero.com>
2024-11-04 08:26:22 +0200

    Finishing the Update Product Code

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,8 @@
 const Product = require('../models/product');
+const mongodb = require("mongodb");
 
+const ObjectId = mongodb.ObjectId;
+console.log(ObjectId, "mongodb")
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
@@ -51,18 +54,11 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    Product.findById(prodId)
-        .then(product => {
-            product.title = updatedTitle;
-            product.price = updatedPrice;
-            product.description = updatedDesc;
-            product.imageUrl = updatedImageUrl;
-            return product.save();
-        })
-        .then(result => {
-            console.log('UPDATED PRODUCT!');
-            res.redirect('/admin/products');
-        })
+    const product = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, new ObjectId(prodId))
+    product.save().then(result => {
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');
+    })
         .catch(err => console.log(err));
 };
 
