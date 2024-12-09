204cc36a1ecc51824ebafb99fe75e64bfbe5e0d7
Howaida Sayed <h.sayed@neoxero.com>
2024-12-09 22:12:34 +0200

    updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -59,16 +59,20 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const product = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, prodId)
-    product.save().then(result => {
+    Product.findById(prodId).then((product) => {
+        product.title = updatedTitle;
+        product.price = updatedPrice;
+        product.imageUrl = updatedImageUrl;
+        Product.description = updatedDesc;
+        return product.save();
+    }).then(result => {
         console.log('UPDATED PRODUCT!');
         res.redirect('/admin/products');
-    })
-        .catch(err => console.log(err));
+    }).catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
+    Product.find()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
--- routes/admin.js
+++ routes/admin.js
@@ -34,12 +34,11 @@ router.get('/add-product', adminController.getAddProduct);
 router.post('/add-product', adminController.postAddProduct);
 
 // // /admin/products => GET
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 
-// router.get('/edit-product/:productId', adminController.getEditProduct);
-
-// router.post('/edit-product', adminController.postEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProduct);
 
 // router.post('/delete-product', adminController.postDeleteProduct);
 
