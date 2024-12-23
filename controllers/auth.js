f47215796c67b322496ce96de0e960e07d4ffaed
Howaida Sayed <h.sayed@neoxero.com>
2024-12-23 07:05:42 +0200

    using the session middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,22 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("cookie")?.split("=")[1] === "true"; // cookie: loggedIn=true
+  console.log(req.session.isLoggedIn, "session");
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
   //console.log(req.get("cookie").split("=")[1], "cookie");
 };
 
 exports.postLogin = (req, res, next) => {
-  // this data is lost after the request is done
-  //req.isLoggedIn = true;
-  // get login data --> email & password
-  // assume the user is logged in & redirect
-
-  res.setHeader("Set-Cookie", "loggedIn=true; httpOnly");
+  // session added to req object by the session middleware
+  req.session.isLoggedIn = true; // add a new property to the session object (saved across request but not users)
   res.redirect("/");
 };
-
 /*
-don't store in req--> because after the request is done, the data is lost
-alternatives 
-------------
-- global variable stored in external file (not recommended)
-because it will be shared across all requests & users
-if the server is restarted, the data will be lost.
-
-- store in cookies (recommended)
-and it will be stored in the browser for every single user
-store in the browser & send it with every request to the server 
-cookies can be set to expire after a certain time
---> expire (session) --> expired when the browser is closed
-disadvantage --> user can edit it from the browser 
-so using session instead can fix this problem 
-
-- store in session (recommended)
-- store in database (recommended)
-- store in localstorage (not recommended)
+session needs cookie to store the session id
+to identify the user
+but the sensitive data is stored on the server
+we can't modify it from the client side
 */
