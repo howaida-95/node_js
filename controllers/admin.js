a65ebe1f06d61a66ba0eec718e5c642f82ed3646
Howaida Sayed <h.sayed@neoxero.com>
2025-01-03 17:56:44 +0200

    Working on route protection

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,10 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
+  // check if the user is authenticated or not first before rendering the page
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login");
+  }
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
