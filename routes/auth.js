561e0258ba70ade442e4ce74250fdc4b934aa382
Howaida Sayed <h.sayed@neoxero.com>
2025-02-14 17:33:29 +0200

    Built-In & Custom Validators

--- routes/auth.js
+++ routes/auth.js
@@ -6,16 +6,27 @@ const { check } = require("express-validator");
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
-
 router.get("/signup", authController.getSignup);
 
-/* adding validation to the route , path + any numbers of middlewares
+/* 
+adding validation to the route , path + any numbers of middlewares
 check(field name)
 */
 router.post(
   "/signup", // path
   // express validator middleware --> this middleware stores errors it found in error object
-  check("email").isEmail().withMessage("Please enter a valid email"),
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    // using custom validation here to check if email is already in use (check specific email)
+    .custom((value, { req }) => {
+      // throw an error when validation fails
+      if (value === "test@test.com") {
+        // we can throw error or test@test.com
+        throw new Error("this email address is forbidden");
+      }
+      return true;
+    }),
   authController.postSignup // controller
 );
 
